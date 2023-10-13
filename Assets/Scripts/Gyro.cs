5372ead226dbfb7b6c2d21effc3a87c0f65a05f7
Ashley Hawkins <536669@escg.ac.uk>
2023-10-13 15:20:01 +0100

    Add display for gravity direction

--- Assets/Scripts/Gyro.cs
+++ Assets/Scripts/Gyro.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Gyro : MonoBehaviour
 {
     // Start is called before the first frame update
+    LineRenderer lr;
     static Quaternion offset = Quaternion.identity;
     public GameObject cube;
     static float gravityMagnitude = 9.81f;
     void Start()
     {
+        lr = GetComponent<LineRenderer>();
         gravityMagnitude = 1;
         Input.gyro.enabled = true;
-        Calibrate();
     }
     private static Quaternion GyroToUnity(Quaternion q)
     {
@@ -30,6 +31,9 @@ public class Gyro : MonoBehaviour
         //print(GyroToUnity(Input.gyro.attitude).eulerAngles);
         // cube.transform.rotation = GyroToUnity(Input.gyro.attitude) * offset;
         Physics2D.gravity = Quaternion.Inverse(Quaternion.Euler(0, 0, (GyroToUnity(Input.gyro.attitude) * offset).eulerAngles.z)) * Vector2.down * gravityMagnitude;
+        var unitGravity = Physics2D.gravity.normalized;
+        lr.SetPosition(0, unitGravity * 0.5f);
+        lr.SetPosition(1, -unitGravity * 0.5f);
         print(Physics2D.gravity);
     }
 }
